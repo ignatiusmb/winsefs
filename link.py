92981f75df879e19dc3c1665f4f3ff56bc5d0b0f
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-01-09 22:04:49 +0700

    Add linker file

---
+++ link.py
@@ -0,0 +1,95 @@
+import json
+import subprocess
+from filecmp import cmp
+from os.path import isdir, islink
+from pathlib import Path
+from shutil import rmtree
+
+SOURCE = Path.cwd().joinpath("setup")  # setup folder in winsefs as source
+HOMEDIR = Path.home()
+
+
+class WSFS:
+    def process(src: Path, dst: Path) -> bool:
+        srctime = src.stat().st_mtime
+        dsttime = dst.stat().st_mtime
+        if srctime < dsttime:
+            print(f"{dst} is newer, replacing {src}")
+            WSFS.clone(dst, src)
+        elif srctime == dsttime:
+            print(f"{src} and {dst} is modified at the exact same time")
+            print(f"Using {src} as source file ~> Migrating {dst} to a symbolic link")
+        elif srctime > dsttime:
+            print(f"{src} is newer, updating {dst}")
+        WSFS.symlink(src, dst)
+
+    def validate(src: Path, dst: Path) -> bool:
+        return True if src == dst.resolve() else False
+
+    def clone(src: Path, dst: Path):
+        print(f"Cloning {src} to {dst}")
+        args = ["xcopy","/g"]
+        if isdir(src):
+            args.append("/i")
+        args.append(str(src))
+        args.append(str(dst))
+        subprocess.run(args, shell=True)
+
+    def symlink(src: Path, dst: Path):
+        if dst.exists():
+            if isdir(dst):
+                rmtree(dst)
+            else:
+                dst.unlink()
+        args = ["mklink"]
+        if isdir(src):
+            args.append("/D")
+        args.append(str(dst))
+        args.append(str(src))
+        subprocess.run(args, shell=True)
+
+
+def process(name: str, src: Path, dst: Path) -> bool:
+    if src.exists():
+        if islink(dst):
+            if WSFS.validate(src, dst):
+                print(f"{f} is already linked!")
+                return True
+            print(f"Symlink corrupted, renewing {f}")
+            WSFS.symlink(src, dst)
+        elif dst.exists():
+            print(f"{HOMEDIR} already has {name}, comparing with {src}")
+            WSFS.process(src, dst)
+        return True
+    elif dst.exists():
+        WSFS.clone(dst, src)
+        WSFS.symlink(src, dst)
+        return True
+    else:
+        print(f"There's no {f} in {SOURCE} or {HOMEDIR}.")
+        return False
+
+
+with open("wsfs.json") as load:
+    skipped = []
+    count = empty = 0
+    data = json.load(load)
+    for f in data["files"]:
+        print(f"\n--> Processing {f}:")
+        if process(f, SOURCE.joinpath(f), HOMEDIR.joinpath(f)):
+            count += 1
+            continue
+        empty += 1
+        skipped.append(f)
+
+    for d in data["dirs"]:
+        print(f"\n--> Processing {d} folder:")
+        if process(d, SOURCE.joinpath(d), HOMEDIR.joinpath(d)):
+            count += 1
+            continue
+        empty += 1
+        skipped.append(d)
+
+    total = len(data["files"]) + len(data["dirs"]) - empty
+    print(f"\n# Finished processing | {count} of {total} successful links")
+    print(f"> Skipped links ({empty}): {skipped if skipped else None}")
